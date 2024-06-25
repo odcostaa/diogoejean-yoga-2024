506aee206f00beadd1e4300bcf934300dec33aa6
odcostaa <165224783+odcostaa@users.noreply.github.com>
2024-06-24 22:50:54 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+function abrir(){
+    document.getElementById('backmenu-id').className = 'backmenu-after'
+    document.getElementById('menuitens-id').className = 'menuitens-after'
+    document.getElementById('menuicon-id').className = 'menuicon-after'
+    document.getElementById('xis-id').className = 'xis-after'
+}
+
+function fechar(){
+    document.getElementById('backmenu-id').className = 'backmenu'
+    document.getElementById('menuitens-id').className = 'menuitens'
+    document.getElementById('menuicon-id').className = 'menuicon'
+    document.getElementById('xis-id').className = 'xis'
+}
